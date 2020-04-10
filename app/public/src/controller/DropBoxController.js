681003fb15f67de48354628ade9d06d8d2738606
Willian Reis <willianmpreis@gmail.com>
2020-04-10 20:07:45 -0300

    Seleciona os itens

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -314,6 +314,8 @@ class DropBoxController
             ${this.getFileIconView(file)}
             <div class="name text-center">${file.name}</div>`;
         
+        this.initEventsLi(li);
+
         return li;    
     }
 
@@ -328,6 +330,42 @@ class DropBoxController
 
                 this.listFilesEl.appendChild(this.getFileView(data, key));
             });
+        });
+    }
+
+    initEventsLi(li)
+    {
+        li.addEventListener('click', e => {
+            
+            if (e.shiftKey) {
+                let firstLi = this.listFilesEl.querySelector('.selected');
+                let lis = li.parentElement.childNodes;
+
+                let indexStart;
+                let indexEnd;
+
+                if (firstLi) {
+                    lis.forEach((el, index) => {
+                        if (firstLi === el) indexStart = index;
+                        if (li === el) indexEnd = index;
+                    });
+                }
+
+                let index = [indexStart, indexEnd].sort();
+
+                lis.forEach((el, i) => {
+                    if (i >= index[0] && i<= index[1]) el.classList.add('selected');
+                });
+
+                return;
+            }
+            if (!e.ctrlKey) {
+                this.listFilesEl.querySelectorAll('li.selected').forEach(el => {
+                    el.classList.remove('selected');
+                })
+            }
+            li.classList.toggle('selected');
+
         });
     }
 }
