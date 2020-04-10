2037a3458d30f22a64eda75cb97b4c8540bfdeae
Willian Reis <willianmpreis@gmail.com>
2020-04-10 15:04:23 -0300

    Projeto funcionando upload

---
+++ app/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var formidable = require('formidable');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', (req, res) => {
+  
+  let form = new formidable.IncomingForm({ //Recuperar o formulário
+    uploadDir: './upload', //Diretorio
+    keepExtensions: true //Manter a extensão
+  });
+
+  form.parse(req, (err, fields, files) => {
+    res.json({
+      files
+    });ll
+  });
+});
+
+module.exports = router;
