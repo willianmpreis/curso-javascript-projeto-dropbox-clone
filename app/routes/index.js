89d8a76ff3a07e3c7f077b7edfb9702cf1d84cf1
Willian Reis <willianmpreis@gmail.com>
2020-04-10 23:11:29 -0300

    Adiciona funcionalidade de download

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/file', (req, res) => {
+  let path = './' + req.query.path;
+  if (!fs.existsSync(path)) {
+    return res.status(404).json({
+      error: "File not found"
+    });
+  }
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.status(400).json({
+        error: err
+      });
+    }
+    res.status(200).end(data);        
+  })
+});
+
 router.delete('/file', (req, res) => {
   let form = new formidable.IncomingForm({ //Recuperar o formulário
     uploadDir: './upload', //Diretorio
@@ -18,18 +36,20 @@ router.delete('/file', (req, res) => {
     
     let path = './' + fields.path;
     
-    if (fs.existsSync(path)) {
-      fs.unlink(path, error => {
-        if (error) {
-          res.status(400),json({error});
-          return;
-        }
-
-        res.json({
-          fields
-        });
+    if (!fs.existsSync(path)) {
+      return res.status(404).json({
+        error: "File not found"
       });
     }
+    fs.unlink(path, error => {
+      if (error) {
+        res.status(400),json({error});
+        return;
+      }
+      res.json({
+        fields
+      });
+    });
   });
 });
 
